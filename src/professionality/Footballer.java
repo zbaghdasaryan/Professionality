c347c9c256db9d0e4be582c7000d29583bebf554
Aram Vanyan <aram.vanyan.a@gmail.com>
2019-12-10 17:42:14 +0400

    add Footballer class

--- src/professionality/Footballer.java
+++ src/professionality/Footballer.java
@@ -2,5 +2,12 @@ package professionality;
 
 public class Footballer {
     private int numberOnShirt;
+    private String nameOfClub;
+    private String nameOnShirt;
+
+
+    Footballer(int numberOnShirt,String nameOfClub,String nameOnShirt ){
+
+    }
 
 }
